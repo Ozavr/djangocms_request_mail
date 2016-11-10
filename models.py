a3838ce507c8f5bf476cb5495d25184d75be0bb1
Max Ozavr <ozavr@me.com>
2016-11-10 21:46:18 +0500

    Simply Django: Create view and repair model

--- models.py
+++ models.py
@@ -6,13 +6,13 @@ class RequestList( models.Model ):
 	Модель базы данных для базы данных в которую записываются данные клиента при запросе
 	'''
 	name = models.CharField( max_length = 20, verbose_name = 'Имя' )
-	phone_number = models.CharField( max_length = 20, verbose_name = 'Номер телефона' default = True )
+	phone = models.CharField( max_length = 20, verbose_name = 'Номер телефона', default = True )
 	description = models.TextField( max_length = 350, verbose_name = 'Содержание запроса' )
-	date = models.DataTimeField( auto_now_add = True, db_index =True )
+	date = models.DateTimeField( auto_now_add = True, db_index =True )
 
 	class Meta:
 		'''
 		Settings for the model
 		'''
-		ordering = [ -posted ]
+		ordering = [ '-date' ]
 		verbose_name = 'Список запросов'
--- views.py
+++ views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.core.urlresolvers import reverse_lazy
+from django.core.mail import send_mail
+from django.contrib.messages.views import SuccessMessageMixin
+from tagilturist import settings
 
-# Create your views here.
+# Our model
+from request_mail.models import RequestList
+
+class CreateRequest( SuccessMessageMixin, CreateView ):
+	'''
+	Controller for recording information of client in list of request and sending 
+	request for the managers
+	Контроллер для создание записи клиента в базе данных и отправки запроса менджерам
+	'''
+	model = RequestList
+	template_name = 'request.html'
+	fields = [ 'name', 'phone_number', 'description' ]
+	success_message = 'Спасибо за вашу заявку, наши менеджеры свяжутся с вами в ближайшое время!'
+	success_url = 'news'
+
+	def form_valid( self, form ):
+		'''
+		sending email request
+		'''
+		output = super( CreateRequest, self ).form_valid( form )
+		request_message = 'Запрос клиента с сайта tagilturist.ru!\n\n' +
+			'Имя: ' + form.instance.name + '\n' +
+			'Телефон: ' + form.instance.phone + '\n' +
+			'Краткая информация запроса: ' +  form.instance.description
+
+		send_mail( 'Запрос клиента', request_message, fail_silently = True )
+		return output
