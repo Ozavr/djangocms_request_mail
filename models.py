7e5062cddba022edc10d5c027fb5485db484db13
Max Ozavr <ozavr@me.com>
2016-11-10 18:36:05 +0500

    For simple django: adding model

--- models.py
+++ models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class RequestList( models.Model ):
+	'''
+	Model of the list clients request 
+	Модель базы данных для базы данных в которую записываются данные клиента при запросе
+	'''
+	name = models.CharField( max_length = 20, verbose_name = 'Имя' )
+	phone_number = models.CharField( max_length = 20, verbose_name = 'Номер телефона' default = True )
+	description = models.TextField( max_length = 350, verbose_name = 'Содержание запроса' )
+	date = models.DataTimeField( auto_now_add = True, db_index =True )
+
+	class Meta:
+		'''
+		Settings for the model
+		'''
+		ordering = [ -posted ]
+		verbose_name = 'Список запросов'
